7cc67bd57521628f742a13f925699acb08e4f3e4
DJ Jordan <dallasjordan@protonmail.com>
2026-07-11 15:29:26 -0300

    Add Business Community fan capability

--- web/businesses/urls.py
+++ web/businesses/urls.py
@@ -6,5 +6,14 @@ from . import views
 app_name = "businesses"
 
 urlpatterns = [
-    path("<slug:slug>/", views.business_detail, name="detail"),
+    path(
+        "<slug:slug>/fan/",
+        views.toggle_business_fan,
+        name="toggle_fan",
+    ),
+    path(
+        "<slug:slug>/",
+        views.business_detail,
+        name="detail",
+    ),
 ]
