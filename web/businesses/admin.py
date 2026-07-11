dd65c4b410c004c21b752b5fef76f4d646b0dd24
DJ Jordan <dallasjordan@protonmail.com>
2026-07-11 17:11:39 -0300

    Add Business Updates capability

--- web/businesses/admin.py
+++ web/businesses/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from .models import BusinessListing
-
+from .models import BusinessListing, BusinessUpdate
 
 @admin.register(BusinessListing)
 class BusinessListingAdmin(admin.ModelAdmin):
@@ -35,6 +35,36 @@ class BusinessListingAdmin(admin.ModelAdmin):
     autocomplete_fields = (
         "owner",
         "discovery_hub",
+    )
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+
+@admin.register(BusinessUpdate)
+class BusinessUpdateAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "business",
+        "author",
+        "is_published",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = (
+        "is_published",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = (
+        "title",
+        "body",
+        "business__name",
+        "author__username",
+    )
+    autocomplete_fields = (
+        "business",
+        "author",
     )
     readonly_fields = (
         "created_at",
